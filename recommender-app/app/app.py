b9a3eb2e3b60b63e8a56db0b7fdd0b008787da03
TrrRdrgz <terry.j.rodriguez@gmail.com>
2020-10-27 13:39:48 -0700

    updated naming convention

--- recommender-app/app/app.py
+++ recommender-app/app/app.py
@@ -29,7 +29,7 @@ FLAGS = tf.compat.v1.app.flags.FLAGS
 app = Flask(__name__)
 api = Api(app)
 
-def get_user_embedding(uid):
+def get_user_embedding(user_id):
     channel = grpc.insecure_channel(FLAGS.server)
     stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
     # Send request
@@ -38,23 +38,23 @@ def get_user_embedding(uid):
     request.model_spec.name = 'user_model'
     request.model_spec.signature_name = 'serving_default'
     request.inputs['string_lookup_1_input'].CopyFrom(
-            tf.make_tensor_proto(uid, shape=[1]))
+            tf.make_tensor_proto(user_id, shape=[1]))
     result = stub.Predict(request, 10.0)  # 10 secs timeout
     embedding = np.array(result.outputs["embedding_1"].float_val)
     return tf.convert_to_tensor(embedding)
 
 class Recommender(Resource):
-    def get(self, uid):
-        user_recs = {"uid": [], "recommendations": []}
-        user = tf.convert_to_tensor(uid, dtype="string")
+    def get(self, user_id):
+        user_recs = {"user_id": [], "recommendations": []}
+        user = tf.convert_to_tensor(user_id, dtype="string")
         query_embedding = get_user_embedding(user)
         candidates = content_index.get_nns_by_vector(query_embedding, top_N)
         candidates = [int(content_index_to_movie[x].decode("utf-8")) for x in candidates]
-        user_recs["uid"].append(user.numpy().decode("utf-8"))
+        user_recs["user_id"].append(user.numpy().decode("utf-8"))
         user_recs["recommendations"].append(candidates)
         return user_recs
 
-api.add_resource(Recommender, '/recommend/<uid>')
+api.add_resource(Recommender, '/recommend/<user_id>')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
